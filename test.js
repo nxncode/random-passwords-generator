60b0fcf014c245b66a3dba784c83b84c5a731c41
Nixon Augustine <nixon_augustine@hotmail.com>
2020-05-24 13:55:26 +0530

    random password generator

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const password = require("./index");
 
 options = {
   LENGTH: 10,
-  ALLOW_ALPHABETS: false,
-  ALLOW_UPPERCASE: true,
-  ALLOW_NUMBERS: true,
-  ALLOW_SPECIAL_CHARACTERS: false,
-  EXCEPTIONS: "",
+  // ALLOW_ALPHABETS: false,
+  // ALLOW_UPPERCASE: true,
+  // ALLOW_NUMBERS: true,
+  // ALLOW_SPECIAL_CHARACTERS: false,
+  // EXCEPTIONS: "",
 };
 passwords = password(options);
 console.log(passwords);
