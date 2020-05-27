404149b9b39b687c1de0e4fa64a91efcbe3b7173
Nixon Augustine <nixon_augustine@hotmail.com>
2020-05-27 08:28:58 +0530

    Added Option for Minimum Characters and First Character

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-const generateString = require("./lib/string-generator.js");
+const passwords = require("./lib/password-generator.js");
 const generateRandomNumber = require("./lib/generate-random-number");
+const errorCheck = require("./lib/check-options-error");
+const processOptions = require("./lib/process-options");
 
 var options = {};
 
 const passwordGenerator = (options) => {
-  //If No parameter is passed as an Option, then default values will be taken.
-  if (typeof options == "undefined") {
-    options = {
-      LENGTH: 10,
-      ALLOW_ALPHABETS: true,
-      ALLOW_UPPERCASE: true,
-      ALLOW_NUMBERS: true,
-      ALLOW_SPECIAL_CHARACTERS: false,
-      EXCEPTIONS: "",
-    };
-  }
-  const charactersString = generateString(options);
+  options = processOptions(options);
 
-  //If the length of the password to be generated is not specified, then the length will be taken as default value of 10
-  options.LENGTH = options.LENGTH || 10;
+  errorCheck(options);
+  const pswd = passwords(options);
 
-  var password = "";
-
-  for (i = 0; i < options.LENGTH; i++) {
-    randomNumber = generateRandomNumber(charactersString);
-    password = password + charactersString[randomNumber];
-  }
-
-  return password;
+  return pswd;
 };
 
 module.exports = passwordGenerator;
